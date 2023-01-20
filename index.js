34381b7e00dab6ec4a8b295693d7c79f3a79d58d
sh0t4r0 <32794764+sh0t4r0@users.noreply.github.com>
2023-01-20 16:57:08 +0000

    Update index.js

--- index.js
+++ index.js
@@ -23,10 +23,10 @@
 			message.channel.sendMessage('merrrrmão! coê viado, posso mandar um papo reto? ixpeira tua veixx pra falar vacilão, bora ali comer um bixxcoito que to bolado.');
 		}
 
-		////se for eu
-		//if(message.author.id == "200240729103007744" && message.channel.id == "367865604994236416") {
-		//	message.channel.sendMessage('É verdade @k4n3d4, concordo plenamente!');
-		//}
+		//se for eu
+		if(message.author.id == "200240729103007744" && message.channel.id == "367865604994236416") {
+			message.channel.sendMessage('É verdade @k4n3d4, concordo plenamente!');
+		}
 
 		//se for eu ping
 		if(message.author.id == "200240729103007744" && message.content == "hhping") {
